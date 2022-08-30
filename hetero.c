9b07f9208c9bef804cf2fe8ee0185a3bd7f01f9b
Bruno Lacerda <bruno.lacerda9393@gmail.com>
2022-08-30 00:15:28 -0300

    upload lista 01 revisited

---
+++ hetero.c
@@ -0,0 +1,153 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX 3
+
+
+    // declaracao das structs
+
+    typedef struct data {
+        int dia;
+        int mes;
+        int ano;
+    } Data;
+
+    typedef struct ficha_aluno {
+        char nome[50];
+        Data data;
+        int  RA;
+    } Aluno;
+
+//-------------------------------------------------------------------------------------------------------------//
+
+    // declaracao das funcoes
+
+    Data    fillData    (int dia, int mes, int ano);
+    Aluno   fillAluno   (char nome[], Data data, int RA);
+    void    showAluno   (Aluno aluno);
+    void    showData    (Data data);
+    int     comparaData (Data data1, Data data2);
+
+
+    // corpo das funcoes
+
+        // retorna uma struct no formato aluno
+
+        Aluno fillAluno(char nome[],
+                        Data data,
+                        int  RA) {
+            Aluno aux;
+            strcpy(aux.nome, nome);
+            aux.data = data;
+            aux.RA = RA;
+            return aux;
+        }
+
+        // exibe um aluno
+        
+        void showAluno(Aluno aluno) {
+            printf("Nome : %s\n", aluno.nome);
+            printf("RA.. : %d\n", aluno.RA);
+            printf("Data : ");
+            showData(aluno.data);
+        }
+
+        // retorna uma struct no formato data
+        
+        Data fillData(int dia,
+                      int mes,
+                      int ano) {
+            Data aux;
+            aux.dia = dia;
+            aux.mes = mes;
+            aux.ano = ano;
+            return aux;
+        }
+
+        // exibe uma data
+        
+        void showData(Data data) {
+            printf("%02d/%02d/%02d\n\n", data.dia, data.mes, data.ano);
+        }
+
+        // compara datas
+        // retorna  0 se as datas são iguais
+        // retorna  1 se a primeira data for maior
+        // retorna -1 se a segunda  data for maior
+
+        int comparaData(Data data1, Data data2) {
+            if (data1.ano == data2.ano) {
+                if (data1.mes == data2.mes) {
+                    if (data1.dia == data2.dia) {
+                        return 0;
+                    } else {
+                        if (data1.dia < data2.dia) {
+                            return -1;
+                        } else {
+                            return 1;
+                        }
+                    } 
+                } else {
+                    if (data1.mes < data2.mes) {
+                        return -1;
+                    } else {
+                        return 1;
+                    }
+                }
+            } else {
+                if (data1.ano < data2.ano) {
+                    return -1;
+                } else {
+                    return 1;
+                }
+            }
+        }
+
+        // le dados do aluno
+        // retorna uma struct com os dados do aluno
+
+        Aluno addAluno() {
+            char nome[50];
+            int dia, mes, ano, RA;
+
+            printf("Nome do aluno: ");
+            fgets(nome,50,stdin);
+            nome[strlen(nome)-1] = 0;
+            printf("RA do aluno: ");
+            scanf("%d", &RA);
+            getchar();
+            printf("Data de nascimento do aluno: ");
+            scanf("%d/%d/%d", &dia, &mes, &ano);
+            while(getchar() != '\n');
+            return fillAluno(nome,
+                             fillData(dia, mes, ano),
+                             RA);
+        }
+
+//-------------------------------------------------------------------------------------------------------------//
+
+    // corpo do programa
+
+    int main() {
+        Aluno alunos[MAX];
+        int dia, mes, ano;
+
+        for (int i=0; i<MAX; i++) {
+            alunos[i] = addAluno();
+        }
+
+        printf("Digite a data de corte: ");
+        scanf("%d/%d/%d", &dia, &mes, &ano);
+        while(getchar() != '\n');
+        
+        for (int i=0; i<MAX; i++) {
+            if(comparaData(alunos[i].data,
+                           fillData(dia,mes,ano)) == 1) {
+                printf("Aluno %d:\n", i+1);
+                showAluno(alunos[i]);
+            }
+        }   
+
+        return 0;
+    }
--- lista01.c
+++ lista01.c
@@ -1,224 +1,207 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "tools.h"
 
-void slice(char array[], int max, char c) {
-    for (int i=0; i<max; i++) {
-        if (array[i] == c) array[i] = '\0';
-    }
-}
 
-int validateTime(int hour, int min, int sec) {
-    if (hour >= 0 && hour < 24) {
-        if (min >= 0 && min < 60) {
-            if (sec >= 0 && sec < 60)
-                return 0;
+// corpo das funcoes
+
+    // substitui o caractere passado como parâmetro por '\0'
+
+    void slice(char array[], int max, char c) {
+        for (int i=0; i<max; i++) {
+            if (array[i] == c) array[i] = '\0';
         }
     }
-    printf("\nDIGITE UMA HORA VALIDA!\n\n");
-    return 1;
-}
 
-void swapAddress (int *n1, int *n2) {
-    int temp;
-    temp = *n1;
-    *n1 = *n2;
-    *n2 = temp;
-}
+    // converte do formato 24h para segundos
 
-void difference(int h1, int h2, int m1, int m2, int s1, int s2) {
-    int h, m, s;
-
-    if (h1 > h2) {
-        swapAddress(&h1, &h2);
-        swapAddress(&m1, &m2);
-        swapAddress(&s1, &s2);
-    }
-    else if (h1 == h2 && m1 > m2) {
-        swapAddress(&m1, &m2);
-        swapAddress(&s1, &s2);
-    }
-    else if ((h1 == h2 && m1 == m2) && (s1 > s2))
-        swapAddress(&s1, &s2);
-
-    if (s1 > s2) {
-        s2 += 60;
-        m2--;
+    int hourToSec(int h, int m, int s) {
+        return h*3600 + m*60 + s;
     }
 
-    if (m1 > m2) {
-        m2 += 60;
-        h2--;
+    // converte segundos para o formato 24h
+
+    void secToHour(int sec) {
+
+        sec = sec>=0 ? sec : sec*-1;
+
+        int r = sec%3600;
+        int h = sec/3600;
+        int m = r/60;
+        int s = r%60;
+
+        printf("\nDiferenca entre as horas digitadas:\n");
+        printf("%02d:%02d:%02d", h, m, s);
     }
 
-    h = h2-h1; m = m2-m1; s = s2-s1;
-    printf("\nDiferenca entre as horas digitadas:\n");
-    printf("%02d:%02d:%02d", h, m, s);
-}
+    // EXERCICIO 01
 
-int hourToSec(int h, int m, int s) {
-    return h*3600 + m*60 + s;
-}
+    void ex_01() {
+        printf("EXERCICIO 01");
+        printf("\n - Criar um programa capaz de calcular o tempo entre dois horarios quaisquer de um determinado dia.");
+        printf("\n - O programa deve ler dois horarios, no formato HH:MM:SS (dois digitos para hora, dois para minutos e dois para segundos). O programa deve verificar se o horario e valido(HH entre 00 e 23, MM entre 00 e 59, SS entre 00 e 59).");
+        printf("\n - O programa deve ter uma funcao para calcular a quantidade de segundos em um horario, e outra funcao para calcular e imprimir a quantidade de horas, minutos e segundos em uma quantidade de segundos\n\n");
 
-void secToHour(int sec) {
+        const int MAX = 10;
+        int h1, h2, m1, m2, s1, s2;
+        char hora1[MAX], hora2[MAX];
 
-    sec = sec>0 ? sec : sec*-1;
+        do { // hast
 
-    int r = sec%3600;
-    int h = sec/3600;
-    int m = r/60;
-    int s = r%60;
+            printf("Digite a primeira hora (hh:mm:ss): ");
+            fgets(hora1, MAX, stdin);
+            slice(hora1, MAX, ':'); h1 = atoi(hora1); m1 = atoi(&hora1[3]); s1 = atoi(&hora1[6]);
 
-    printf("\nDiferenca entre as horas digitadas:\n");
-    printf("%02d:%02d:%02d", h, m, s);
-}
+        } while (validateTime(h1, m1, s1));
 
-void ex_01() {
-    printf("EXERCICIO 01");
-    printf("\n - Criar um programa capaz de calcular o tempo entre dois horarios quaisquer de um determinado dia.");
-    printf("\n - O programa deve ler dois horarios, no formato HH:MM:SS (dois digitos para hora, dois para minutos e dois para segundos). O programa deve verificar se o horario e valido(HH entre 00 e 23, MM entre 00 e 59, SS entre 00 e 59).");
-    printf("\n - O programa deve ter uma funcao para calcular a quantidade de segundos em um horario, e outra funcao para calcular e imprimir a quantidade de horas, minutos e segundos em uma quantidade de segundos\n\n");
+        do { // hast
 
-    const int MAX = 10;
-    int h1, h2, m1, m2, s1, s2;
-    char hora1[MAX], hora2[MAX];
+            printf("Digite a segunda  hora (hh:mm:ss): ");
+            fgets(hora2, MAX, stdin);
+            slice(hora2, MAX, ':'); h2 = atoi(hora2); m2 = atoi(&hora2[3]); s2 = atoi(&hora2[6]);
 
-    do { //hast
-        printf("Digite a primeira hora (hh:mm:ss): ");
-        fflush(stdin); fgets(hora1, MAX, stdin);
-        slice(hora1, MAX, ':'); h1 = atoi(hora1); m1 = atoi(&hora1[3]); s1 = atoi(&hora1[6]);
-    } while (validateTime(h1, m1, s1));
+        } while (validateTime(h2, m2, s2));
 
-    do { //hast
-        printf("Digite a segunda  hora (hh:mm:ss): ");
-        fflush(stdin); fgets(hora2, MAX, stdin);
-        slice(hora2, MAX, ':'); h2 = atoi(hora2); m2 = atoi(&hora2[3]); s2 = atoi(&hora2[6]);
-    } while (validateTime(h2, m2, s2));
+        secToHour(hourToSec(h1, m1, s1) - hourToSec(h2, m2, s2));
 
-    //difference(h1, h2, m1, m2, s1, s2);
+        printf("\n\n");
+    }
 
-    secToHour(hourToSec(h1, m1, s1) - hourToSec(h2, m2, s2));
+    // EXERCICIO 02
 
-    printf("\n\n");
-}
+    void ex_02() {
+        printf("EXERCICIO 02");
+        printf("\n - Faca um programa que, dadas 2 palavras, determine:");
+        printf("\n - Se as palavras sao iguais;");
+        printf("\n - Caso as palavras sejam diferentes, qual delas tem maior comprimento (nao esquecer a possibilidade de existirem palavras diferentes de mesmo tamanho);");
+        printf("\n - Verifique se a segunda palavra e uma sub string da primeira:");
+        printf("\n - Exemplo:\tPalavra 1 = casamento\n\t\tPalavra 2 = casa\n\n");
 
-void ex_02() {
-    printf("EXERCICIO 02");
-    printf("\n - Faca um programa que, dadas 2 palavras, determine:");
-    printf("\n - Se as palavras sao iguais;");
-    printf("\n - Caso as palavras sejam diferentes, qual delas tem maior comprimento (nao esquecer a possibilidade de existirem palavras diferentes de mesmo tamanho);");
-    printf("\n - Verifique se a segunda palavra e uma sub string da primeira:");
-    printf("\n - Exemplo:\tPalavra 1 = casamento\n\t\tPalavra 2 = casa\n\n");
+        const int MAX = 20;
+        char *p1, *p2;
+        char word1[MAX], word2[MAX];
+        int sub = 0;
+        
+        printf("Digite  uma  palavra: ");
+        scanf("%s", &word1); cleanBuffer();
+        printf("Digite outra palavra: ");
+        scanf("%s", &word2); cleanBuffer();
 
-    const int MAX = 20;
-    char *p1, *p2;
-    char word1[MAX], word2[MAX];
-    int sub = 0;
-    
-    printf("Digite  uma  palavra: ");
-    fflush(stdin); scanf("%s", &word1);
-    printf("Digite outra palavra: ");
-    fflush(stdin); scanf("%s", &word2);
+        p1 = strlwr(word1);                                         // funciona apenas em sistemas windows
+        p2 = strlwr(word2);                                         // funciona apenas em sistemas windows
+        
+        printf("\n");
 
-    p1 = strlwr(word1);
-    p2 = strlwr(word2);
-    printf("\n");
+        if (strcmp(p1, p2) == 0)
+            printf("As palavras sao iguais!");
+        else {
+            if (strlen(word1) > strlen(word2)) {
+                printf("A palavra [%s] e maior do que a palavra [%s]", word1, word2);
 
-    if (strcmp(p1, p2) == 0)
-        printf("As palavras sao iguais!");
-    else {
-        if (strlen(word1) > strlen(word2)) {
-            printf("A palavra [%s] e maior do que a palavra [%s]", word1, word2);
+                // testa se é substring
 
-            // testa se é substring
-            for (int i=0; p1[i] != '\0'; i++) {
-                sub = 0;
-                for (int j=0; p2[j] != '\0'; j++) {
-                    if (p1[i+j] != p2[j]) {
-                        sub = 0;
+                for (int i=0; p1[i] != '\0'; i++) {
+                    sub = 0;
+                    for (int j=0; p2[j] != '\0'; j++) {
+                        if (p1[i+j] != p2[j]) {
+                            sub = 0;
+                            break;
+                        }
+                        sub = 1;
+                    }
+                    if (sub) {
                         break;
                     }
-                    sub = 1;
-                }
-                if (sub) {
-                    break;
                 }
             }
+            else if (strlen(word2) > strlen(word1))
+                printf("A palavra [%s] e maior do que a palavra [%s]", word2, word1);
+            else
+                printf("Ambas possuem o mesmo tamanho!");
+            
+            // bloco das substrings
+            if (sub)
+                printf("\n\nA palavra [%s] e uma substring de [%s]", word2, word1);
+            else 
+                printf("\n\nA palavra [%s] nao e substring de [%s]", word2, word1);
         }
-        else if (strlen(word2) > strlen(word1))
-            printf("A palavra [%s] e maior do que a palavra [%s]", word2, word1);
-        else
-            printf("Ambas possuem o mesmo tamanho!");
+
+        printf("\n\n");
+    }
+
+    // EXERCICIO 03
+
+    void ex_03() {
+        printf("EXERCICIO 03");
+        printf("\n - Faca um programa que leia 10 nomes completos do teclado, e ao final apresente uma listagem dos nomes no formato:");
+        printf("\n - Sobrenome, Primeiro_Nome\n\n");
+
+        char nomes[10][50];
+        char *nome[10];
+        char *sobreNome[10];
+
+        printf("Digite 10 nomes!\n\n");
         
-        // Bloco das substrings
-        if (sub)
-            printf("\n\nA palavra [%s] e uma substring de [%s]", word2, word1);
-        else 
-            printf("\n\nA palavra [%s] nao e substring de [%s]", word2, word1);
-    }
-    printf("\n\n");
-}
-
-void ex_03() {
-    printf("EXERCICIO 03");
-    printf("\n - Faca um programa que leia 10 nomes completos do teclado, e ao final apresente uma listagem dos nomes no formato:");
-    printf("\n - Sobrenome, Primeiro_Nome\n\n");
-
-    char nomes[10][30];
-    char *nome[10];
-    char *sobreNome[10];
-    char *p;
-
-    printf("Digite 10 nomes!\n\n");
-    for (int i=0; i<10; i++) {
-        printf("Digite o %2d nome: ", i+1);
-        fflush(stdin); fgets(nomes[i], 30, stdin);
-        for (int j=0; j<30; j++) {
-            if (nomes[i][j] == '\n')
-                nomes[i][j] = '\0';
+        for (int i=0; i<10; i++) {
+            printf("Digite o %2d nome: ", i+1);
+            fgets(nomes[i], 50, stdin);
+            
+            for (int j=0; j<50; j++) {
+                if (nomes[i][j] == '\n')
+                    nomes[i][j] = '\0';
+            }
         }
+
+        for (int i=0; i<10; i++) {
+            nome[i] = strtok(nomes[i], " ");
+            sobreNome[i] = strtok(NULL, "\0");
+        }
+
+        for (int i=0; i<10; i++) {
+            printf("\nPessoa %2d: %s, %s", i+1, sobreNome[i], nome[i]);
+        }
+
+        printf("\n\n");
     }
 
-    for (int i=0; i<10; i++) {
-        p = strtok(nomes[i], " ");
-        nome[i] = p;
-        p = strtok(NULL, "\0");
-        sobreNome[i] = p;
-    }
+    // menu de decisao
 
-    for (int i=0; i<10; i++) {
-        printf("\nPessoa %2d: %s, %s", i+1, sobreNome[i], nome[i]);
-    }
-
-    printf("\n\n");
-}
-
-void menu(char opc) {
-    system("cls");
-    switch (opc) {
-        case '1': ex_01();           break;
-        case '2': ex_02();           break;
-        case '3': ex_03();           break;
-        default: printf("INVALID!\n\n"); break;
-    }
-}
-
-int main() {
-    char opc;
-    do { //hast
+    void menu(char opc) {
         system("cls");
-        printf("LISTA 01");
-        printf("\n 1 - Exercicio 01");
-        printf("\n 2 - Exercicio 02");
-        printf("\n 3 - Exercicio 03");
-        printf("\n 0 - Exit");
-        printf("\nChoose: ");
-        
-        fflush(stdin); scanf("%c", &opc);
-        if (opc != '0') 
-            menu(opc);
-        system("pause");
+        switch (opc) {
+            case '1': ex_01();               break;
+            case '2': ex_02();               break;
+            case '3': ex_03();               break;
+            default: printf("INVALID!\n\n"); break;
+        }
+    }
 
-    } while (opc != '0');
-    return 0;
-}
+//-------------------------------------------------------------------------------------------------------------//
+
+    // corpo do programa
+
+    int main() {
+        char opc;
+
+        do { // hast
+            system("cls");
+
+            printf("LISTA 01\n");
+            printf("\n 1 - Exercicio 01");
+            printf("\n 2 - Exercicio 02");
+            printf("\n 3 - Exercicio 03");
+            printf("\n 0 - Sair\n");
+            printf("\nEscolha: ");
+            
+            scanf("%c", &opc); cleanBuffer();
+            
+            if (opc != '0') 
+                menu(opc);
+
+            system("pause");
+
+        } while (opc != '0');
+        
+        return 0;
+    }
